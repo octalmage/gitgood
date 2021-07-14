d57fa63c9643e5d0443ea6e783cdf8a446f69f09
Alexa <alexa.sparks@wpengine.com>
2021-07-14 15:53:42 -0500

    chore: add generated directory to .gitignore

--- vue/src/store/generated/octalmage/gitgood/octalmage.gitgood.gitgood/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Achievement } from "./module/types/gitgood/achievement";
-import { Goal } from "./module/types/gitgood/goal";
-import { Stat } from "./module/types/gitgood/stat";
-import { Team } from "./module/types/gitgood/team";
-export { Achievement, Goal, Stat, Team };
-declare const _default;
-export default _default;
