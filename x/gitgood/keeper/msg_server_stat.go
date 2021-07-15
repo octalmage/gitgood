9c30d06fd310dd53969f426bcba02d218527be2e
Jason Stallings <jason@stallin.gs>
2021-07-15 09:57:05 -0600

    feat: Payout on goal completion!

--- x/gitgood/keeper/msg_server_stat.go
+++ x/gitgood/keeper/msg_server_stat.go
@@ -2,16 +2,72 @@ package keeper
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/bech32"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	"github.com/octalmage/gitgood/x/gitgood/types"
+	"github.com/tendermint/tendermint/crypto"
 )
 
 func (k msgServer) CreateStat(goCtx context.Context, msg *types.MsgCreateStat) (*types.MsgCreateStatResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	goals := k.GetAllGoal(ctx)
+
+	var goal types.Goal
+	var found bool
+
+	for _, v := range goals {
+		if int32(v.Id) == msg.StatType {
+			goal = v
+			found = true
+		}
+	}
+
+	if !found {
+		return nil, errors.New("bad goal")
+	}
+
+	var comparison bool
+
+	if goal.Comparison == 0 {
+		comparison = msg.Final >= msg.Initial
+	} else if goal.Comparison == 1 {
+		comparison = msg.Final > msg.Initial
+	}
+
+	if comparison {
+		feeCoins, err := sdk.ParseCoinsNormalized(fmt.Sprintf("%dexp", goal.Exp))
+		if err != nil {
+			return nil, err
+		}
+
+		// mint fresh exp to the mint module.
+		if err := k.bankKeeper.MintCoins(ctx, minttypes.ModuleName, feeCoins); err != nil {
+			return nil, err
+		}
+
+		// TODO: Use team name instead of team ID.
+		teamAcct := sdk.AccAddress(crypto.AddressHash([]byte(msg.Owner)))
+
+		bech32PrefixAccAddr := sdk.GetConfig().GetBech32AccountAddrPrefix()
+
+		add, err := bech32.ConvertAndEncode(bech32PrefixAccAddr, crypto.AddressHash([]byte(msg.Owner)))
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println(add)
+
+		// exp to team
+		if err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, teamAcct, feeCoins); err != nil {
+			return nil, err
+		}
+	}
 
 	var stat = types.Stat{
 		Creator:   msg.Creator,
